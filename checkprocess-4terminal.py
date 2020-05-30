457259836691672cc95d38d00299c9832c9de123
Linuxkay <sccck000@gmail.com>
2020-05-31 08:00:12 +0900

    new file added for backup

---
+++ checkprocess-4terminal.py
@@ -0,0 +1,9 @@
+import commands
+import os
+while True:
+  output = commands.getoutput('ps -aux')
+  if '/usr/bin/python2.7 /home/user/Workspace/silenceisgolden.py' in output:
+#    print "ITS ALIVE!!!"
+    continue 
+  else: 
+    os.system('/usr/bin/python2.7 /home/user/Workspace/silenceisgolden.py &')
